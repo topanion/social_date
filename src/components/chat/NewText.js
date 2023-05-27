95efdcca5e9212a21117a8853c19ccc599fab0bf
topanion <nj-cs@hotmail.com>
2023-05-27 17:07:28 +0200

    same but different values

--- src/components/chat/NewText.js
+++ src/components/chat/NewText.js
@@ -21,7 +21,7 @@ export default function NewText({ sendMessage }) {
           value={body}
           autocomplete="off"
           onChange={(e) => setBody(e.target.value)}
-          className="flex-auto px-3 py-2 text-base h-[7vh] min-h-[20px] rounded-full bg-gray-400 focus:border-[#222226] focus:outline-none text-white placeholder-white"
+          className="flex-auto px-3 py-2 text-base h-[4vh] min-h-[30px] rounded-full bg-gray-400 focus:border-[#222226] focus:outline-none text-white placeholder-white"
         />
         <button
           type="submit"
