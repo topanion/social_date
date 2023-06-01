6b4004fac188046b2ed064f1d16cfca1b21ba533
topanion <nj-cs@hotmail.com>
2023-06-01 16:25:59 +0200

    changed next config so that it can accept supabase storage as a source for image

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-     domains: ['lh3.googleusercontent.com']
+    domains: [
+      "lh3.googleusercontent.com",
+      "https://stilftnvhmqkbysfvptr.supabase.co/",
+    ],
   },
 };
 
