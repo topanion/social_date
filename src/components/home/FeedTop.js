7a072c64d71c5f64bce52ff627306d1bae6181ee
topanion <nj-cs@hotmail.com>
2023-06-01 16:16:13 +0200

    feed top is now using profile and not user to get avatar_url

--- src/components/home/FeedTop.js
+++ src/components/home/FeedTop.js
@@ -4,7 +4,9 @@ import { useRouter } from "next/router";
 
 export default function FeedTop({ user, profile }) {
   const router = useRouter();
-  const image_source = user.avatar_url ? user.avatar_url : "/profile/blank.png";
+  const image_source = profile.avatar_url
+    ? profile.avatar_url
+    : "/profile/blank.png";
 
   return (
     <div className="fixed top-0 bg-white w-full p-1 border-b-2 flex flex-row gap-2 items-center">
