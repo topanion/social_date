d8b6cbd1217de69f355ff0c6c5c859f3cd935e93
topanion <nj-cs@hotmail.com>
2023-06-01 16:27:58 +0200

    another next config change

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "https://stilftnvhmqkbysfvptr.supabase.co/",
-    ],
+    domains: ["lh3.googleusercontent.com", "stilftnvhmqkbysfvptr.supabase.co"],
   },
 };
 
