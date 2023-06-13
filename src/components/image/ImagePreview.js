4a9edd7dfc67fe3870731d26f8da7847b0324a42
topanion <nj-cs@hotmail.com>
2023-06-13 17:11:41 +0200

    replacing X ascii by Close to cose image modals

--- src/components/image/ImagePreview.js
+++ src/components/image/ImagePreview.js
@@ -7,12 +7,12 @@ export default function ImagePreview({ source }) {
   return (
     <>
       {open ? (
-        <div className="fixed top-0 left-0 h-screen w-screen z-50 bg-black flex p-1">
+        <div className="fixed top-0 left-0 h-screen w-screen z-40 bg-black flex p-1">
           <div
-            className="fixed z-40 right-2 top-2 rounded-full h-[30px] w-[30px] p-1"
+            className="fixed z-40 right-6 top-2 rounded-full h-[30px] w-[30px] p-1"
             onClick={() => setOpen(false)}
           >
-            <span className="z-50 m-auto">✖</span>
+            <span className="z-50 m-auto text-white">Close</span>
           </div>
           <div className="relative w-[90vw] h-[60vh] m-auto">
             <div>
